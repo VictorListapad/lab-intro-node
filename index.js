da88eb225ac0548e305289b114e7d407c4e4e2dd
Victor Listapad <victorlistapadjob@gmail.com>
2021-11-09 14:29:21 -0500

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,80 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  // max() {
+  //   if (this.items.length > 0) {
+  //     let highest = this.items[0];
+  //     for (let i = 1; i < this.items.length; i++) {
+  //       if (this.items[i] > highest) {
+  //         highest = this.items[i];
+  //       }
+  //     }
+  //     return highest;
+  //   } else {
+  //     throw new Error("EmptySortedList");
+  //   }
+  // }
 
-  min() {}
+  min() {
+    // if (this.items.length > 0) {
+    //   let lowest = this.items[0];
+    //   for (let i = 1; i < this.items.length; i++) {
+    //     if (this.items[i] < lowest) {
+    //       lowest = this.items[i];
+    //     }
+    //   }
+    //   return lowest;
+    // } else {
+    //   throw new Error("EmptySortedList");
+    // }
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let total = this.items.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    return total;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let total = this.items.reduce((acc, cur) => {
+        return acc + cur;
+      }, 0);
+      return total / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
